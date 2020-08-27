c0aedc4dec499103f741be35ba770b7fd01474af
Ryan Larson <ryalarson11@gmail.com>
2020-08-27 15:10:26 -0500

    Clean alignment class methods and add initial stubs for real juice

--- src/alignment.py
+++ src/alignment.py
@@ -11,6 +11,7 @@ predictor = dlib.shape_predictor(DEFAULT_LANDMARKS_PATH)
 one_path  = join(abspath(dirname(__file__)), "imgs", "before_align.jpg")
 two_path  = join(abspath(dirname(__file__)), "imgs", "after_align.jpg")
 one       = cv2.imread(one_path)
+two = cv2.imread(two_path)
 
 
 class Alignment:
@@ -19,10 +20,16 @@ class Alignment:
     self.detector  = dlib.get_frontal_face_detector()
     self.predictor = dlib.shape_predictor(landmarks_path)
 
-  def distance(self, a: np.ndarray, b: np.ndarray) -> float:
+  def _distance(self, a: np.ndarray, b: np.ndarray) -> float:
     return np.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
 
-  def cosine_formula(self, length_line1, length_line2, length_line3):
+  def _is_between(self, pt1, pt2, pt3, extra_pt) -> bool:
+    c1 = (pt2[0] - pt1[0]) * (extra_pt[1] - pt1[1]) - (pt2[1] - pt1[1]) * (extra_pt[0] - pt1[0])
+    c2 = (pt3[0] - pt2[0]) * (extra_pt[1] - pt2[1]) - (pt3[1] - pt2[1]) * (extra_pt[0] - pt2[0])
+    c3 = (pt1[0] - pt3[0]) * (extra_pt[1] - pt3[1]) - (pt1[1] - pt3[1]) * (extra_pt[0] - pt3[0])
+    return (((c1 < 0) & (c2 < 0) & (c3 < 0)) | ((c1 > 0) & (c2 > 0) & (c3 > 0)))
+
+  def _cosine_formula(self, length_line1, length_line2, length_line3):
     cos_a = -(length_line3 ** 2 - length_line2 ** 2 - length_line1 ** 2) / (2 * length_line2 * length_line1)
     return cos_a
 
@@ -38,96 +45,70 @@ class Alignment:
     qy     = oy + np.sin(angle) * (px - ox) + np.cos(angle) * (py - oy)
     return qx, qy
 
-  def is_between(self, point1, point2, point3, extra_point) -> bool:
-    c1 = (point2[0] - point1[0]) * (extra_point[1] - point1[1]) - (point2[1] - point1[1]) * (extra_point[0] - point1[0])
-    c2 = (point3[0] - point2[0]) * (extra_point[1] - point2[1]) - (point3[1] - point2[1]) * (extra_point[0] - point2[0])
-    c3 = (point1[0] - point3[0]) * (extra_point[1] - point3[1]) - (point1[1] - point3[1]) * (extra_point[0] - point3[0])
-    return (((c1 < 0) & (c2 < 0) & (c3 < 0)) | ((c1 > 0) & (c2 > 0) & (c3 > 0)))
-
-  def align(self, image: np.ndarray):
+  def detect_face(self, image: np.ndarray):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     detections = self.detector(gray, 1)
 
-    for rect in detections:
-      x, y, w, h = rect.left(), rect.top(), rect.right(), rect.bottom()
+    if (len(detections) == 0):
+      return None, None
+    else:
+      rect = detections[0]
       shape = self.predictor(gray, rect)
       shape_arr = shape_to_np(shape)
+      return rect, shape_arr
 
-      nose = shape_arr[27:36].copy().mean(axis=0).astype('int32')
-      left_eye = shape_arr[42:48].copy().mean(axis=0).astype('int32')
-      right_eye = shape_arr[36:42].copy().mean(axis=0).astype('int32')
+  def align_face(self, image: np.ndarray):
+    # http://datahacker.rs/010-how-to-align-faces-with-opencv-in-python/
+    rect, shape_arr = self.detect_face(image)
+    x, y, w, h = rect.left(), rect.top(), rect.right(), rect.bottom()
+    nose       = shape_arr[27:36].copy().mean(axis=0).astype('int32')
+    left_eye   = shape_arr[42:48].copy().mean(axis=0).astype('int32')
+    right_eye  = shape_arr[36:42].copy().mean(axis=0).astype('int32')
 
-      center_of_forehead = ((left_eye[0] + right_eye[0]) // 2, (left_eye[1] + right_eye[1]) // 2)
-      center_pred = (int((x + w) / 2), int((y + y) / 2))
+    center_of_forehead = ((left_eye[0] + right_eye[0]) // 2, (left_eye[1] + right_eye[1]) // 2)
+    center_pred = (int((x + w) / 2), int((y + y) / 2))
 
-      line1 = self.distance(center_of_forehead, nose)
-      line2 = self.distance(center_pred, nose)
-      line3 = self.distance(center_pred, center_of_forehead)
-      cos_a = self.cosine_formula(line1, line2, line3)
-      angle = np.arccos(cos_a)
+    line1 = self._distance(center_of_forehead, nose)
+    line2 = self._distance(center_pred, nose)
+    line3 = self._distance(center_pred, center_of_forehead)
+    cos_a = self._cosine_formula(line1, line2, line3)
+    angle = np.arccos(cos_a)
 
-      rotated_point = self.rotate_point(nose, center_of_forehead, angle)
-      rotated_point = (int(rotated_point[0]), int(rotated_point[1]))
-      if self.is_between(nose, center_of_forehead, center_pred, rotated_point):
-        angle = np.degrees(-angle)
-      else:
-        angle = np.degrees(angle)
-      # print(center_of_forehead, center_pred, cos_a, angle, rotated_point, angle)
+    rotated_point = self.rotate_point(nose, center_of_forehead, angle)
+    rotated_point = (int(rotated_point[0]), int(rotated_point[1]))
+    if self._is_between(nose, center_of_forehead, center_pred, rotated_point):
+      angle = np.degrees(-angle)
+    else:
+      angle = np.degrees(angle)
 
-      return self.rotate_image(image, nose, angle)
+    eye_delta = self._distance(left_eye, right_eye)
+    # return self.rotate_image(image, nose, angle), eye_delta
+    return image, eye_delta
 
-def rotation_detection_dlib(img, mode, show=False):
-  detector = dlib.get_frontal_face_detector()
-  predictor = dlib.shape_predictor('shape_predictor_5_face_landmarks.dat')
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  rects = detector(gray, 0)
-  if len(rects) > 0:
-    for rect in rects:
-      x = rect.left()
-      y = rect.top()
-      w = rect.right()
-      h = rect.bottom()
+  def scale_faces(self, first: np.ndarray, second: np.ndarray):
+    # http://datahacker.rs/010-how-to-align-faces-with-opencv-in-python/
+    # doesnt assume both images have been aligned first.
+    first_align, first_delta   = self.align_face(first)
+    second_align, second_delta = self.align_face(second)
 
-      shape = predictor(gray, rect)
-      shape = shape_to_normal(shape)
+    # first image will be used as a reference
+    resize_ratio = first_delta / second_delta
+    print(f"d1={first_delta}, d2={second_delta}, r={resize_ratio}")
 
-      nose, left_eye, right_eye = get_eyes_nose_dlib(shape)
+    height_og, width_og = second_align.shape[:2]
+    resize_dim = (int(height_og * resize_ratio), int(width_og * resize_ratio))
+    resize_second = cv2.resize(second_align, resize_dim)
 
-      center_of_forehead = ((left_eye[0] + right_eye[0]) // 2, (left_eye[1] + right_eye[1]) // 2)
-      center_pred = (int((x + w) / 2), int((y + y) / 2))
-
-      length_line1 = distance(center_of_forehead, nose)
-      length_line2 = distance(center_pred, nose)
-      length_line3 = distance(center_pred, center_of_forehead)
-
-      cos_a = cosine_formula(length_line1, length_line2, length_line3)
-
-      angle = np.arccos(cos_a)
-      rotated_point = rotate_point(nose, center_of_forehead, angle)
-      rotated_point = (int(rotated_point[0]), int(rotated_point[1]))
-
-      if is_between(nose, center_of_forehead, center_pred, rotated_point):
-        angle = np.degrees(-angle)
-      else:
-        angle = np.degrees(angle)
-
-      if mode:
-        img = rotate_opencv(img, nose, angle)
-      else:
-        img = np.array(img.rotate(angle))
-    if show:
-      show_img(img)
-    return img
-  else:
-    return img
-
+    comparison = np.hstack([first_align, resize_second])
+    return comparison
 
 
 
 
 
 alignment = Alignment(DEFAULT_LANDMARKS_PATH)
-test_one = alignment.align(one)
+# test_one, test_delta = alignment.align_face(one)
+test_one = alignment.scale_faces(one, two)
 cv2.imshow('Test alignmnet', test_one)
 while True:
   ch = cv2.waitKey(1)
